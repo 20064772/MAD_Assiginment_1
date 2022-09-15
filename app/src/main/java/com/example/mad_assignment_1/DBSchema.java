fe59d04520f351e6d3e4ceb731c04a892a027f7a
19836511 <19836511@student.curtin.edu.au>
2022-09-15 14:58:49 +0800

    Created database schema class

---
+++ app/src/main/java/com/example/mad_assignment_1/DBSchema.java
@@ -0,0 +1,11 @@
+package com.example.mad_assignment_1;
+
+/**
+ * Database Schema Class - Contains inner classes for each table and the columns in those tables
+ *
+ * @author Tristan S. Tutungis
+ */
+public class DBSchema
+{
+
+}
