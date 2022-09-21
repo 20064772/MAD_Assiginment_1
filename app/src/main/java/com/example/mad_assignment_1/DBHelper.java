ef07a86c924864244b59677a6a3211ceeed1a2d0
Tristan S. Tutungis <19836511@student.curtin.edu.au>
2022-09-21 13:02:24 +0800

    Edited Database classes

--- app/src/main/java/com/example/mad_assignment_1/DBHelper.java
+++ app/src/main/java/com/example/mad_assignment_1/DBHelper.java
@@ -14,12 +14,12 @@ import androidx.annotation.RequiresApi;
  * @inherits        android.database.sqlite.SQLiteOpenHelper
  * @author          Tristan S. Tutungis
  * @date_created    15/09/2022
- * @last_modified   15/09/2022 22:21
+ * @last_modified   16/09/2022 22:10
  */
 public class DBHelper extends SQLiteOpenHelper
 {
-    private static final int VERSION = 1;
-    private static final String DB_NAME = "Food.db";
+    private static final int VERSION = 1;               //Database version
+    private static final String DB_NAME = "Food.db";    //Database name
 
     @RequiresApi(api = Build.VERSION_CODES.P)
     public DBHelper(Context context)
@@ -27,13 +27,46 @@ public class DBHelper extends SQLiteOpenHelper
         super(context, DB_NAME, VERSION, null);
     }
 
+    /**
+     * Creates the database schema
+     *
+     * @param db (SQLiteDatabase) - The database object
+     */
     @Override
     public void onCreate(SQLiteDatabase db)
     {
+        /* Turns on foreign key constraints */
+        db.execSQL("PRAGMA foreign_keys = ON");
+    
+        /* Creates User Table */
         db.execSQL("CREATE TABLE " + UserTable.NAME + " (" +
                 UserTable.Columns.EMAIL + " TEXT PRIMARY KEY, " +
-                UserTable.Columns.PASS + " TEXT) ");
+                UserTable.Columns.PASS + " TEXT) "
+        );
 
+        /* Creates Restaurant Table */
+        db.execSQL("CREATE TABLE " + RestaurantTable.NAME + " (" +
+                RestaurantTable.Columns.REST + " TEXT PRIMARY KEY, " +
+                RestaurantTable.Columns.LOGO + " TEXT) "
+        );
+
+        /* Creates Menu Table */
+        db.execSQL("CREATE TABLE " + MenuTable.NAME + " (" +
+                MenuTable.Columns.ITEM + " TEXT, " +
+                MenuTable.Columns.REST + " TEXT, " +
+                MenuTable.Columns.IMG + " TEXT, " +
+                MenuTable.Columns.DESC + " TEXT, " +
+                MenuTable.Columns.PRICE + " REAL, " +
+                "PRIMARY KEY (" +
+                        MenuTable.Columns.ITEM + ", " +
+                        MenuTable.Columns.REST + "), " +
+                "FOREIGN KEY (" + MenuTable.Columns.REST +
+                        ") REFERENCES " + RestaurantTable.NAME +
+                        " (" + RestaurantTable.Columns.REST +
+                        ") ON DELETE CASCADE ON UPDATE NO ACTION )"
+        );
+
+        /* Creates Order History Table */
         db.execSQL("CREATE TABLE " + OrderHistoryTable.NAME + " (" +
                 OrderHistoryTable.Columns.USER + " TEXT, " +
                 OrderHistoryTable.Columns.DATETIME + " TEXT, " +
@@ -50,8 +83,19 @@ public class DBHelper extends SQLiteOpenHelper
                 "FOREIGN KEY (" + OrderHistoryTable.Columns.ITEM +
                         ") REFERENCES " + MenuTable.NAME +
                         " (" + MenuTable.Columns.ITEM +
-                        ") ON DELETE CASCADE ON UPDATE NO ACTION )" );
+                        ") ON DELETE CASCADE ON UPDATE NO ACTION )"
+        );
 
     }
+    
+    /**
+     * Upgrades database schema
+     *
+     * @param db (SQLiteDatabase) - The database object
+     * @param i (int) - Old version number
+     * @param i1 (int) - New version number
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {}
 
 }
--- app/src/main/java/com/example/mad_assignment_1/DBSchema.java
+++ app/src/main/java/com/example/mad_assignment_1/DBSchema.java
@@ -6,12 +6,15 @@ package com.example.mad_assignment_1;
  * @class           DBSchema
  * @author          Tristan S. Tutungis
  * @date_created    15/09/2022
- * @last_modified   15/09/2022 22:21
+ * @last_modified   16/09/2022 22:10
  */
 
 public class DBSchema
 {
 
+    /**
+     * User Table - Contains columns for a user's email and password
+     */
     public static class UserTable
     {
         public static final String NAME = "users";
@@ -21,7 +24,12 @@ public class DBSchema
             public static final String PASS = "password";
         }
     }
-
+    
+    /**
+     * Order History Table - contains columns for Username (email), date & time,
+     *  item, and quantity ordered. Generally, items with an equal
+     */
+    
     public static class OrderHistoryTable
     {
         public static final String NAME = "order_history";
@@ -30,7 +38,7 @@ public class DBSchema
             public static final String USER = "user";
             public static final String DATETIME = "date & time";
             public static final String ITEM = MenuTable.Columns.ITEM;
-            public static final String QTY = "quantity"
+            public static final String QTY = "quantity";
         }
     }
 
@@ -39,8 +47,8 @@ public class DBSchema
         public static final String NAME = "menu";
         public static class Columns
         {
-            public static final String REST = RestaurantTable.Columns.REST;
             public static final String ITEM = "item";
+            public static final String REST = RestaurantTable.Columns.REST;
             public static final String IMG = "image";
             public static final String DESC = "description";
             public static final String PRICE = "price";
