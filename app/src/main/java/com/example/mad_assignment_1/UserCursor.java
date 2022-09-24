10d3e9bad6c4d915ea24d8eedb285d22ca6fcd87
Tristan S. Tutungis <19836511@student.curtin.edu.au>
2022-09-24 22:43:21 +0800

    Wrote User Cursor class

--- app/src/main/java/com/example/mad_assignment_1/UserCursor.java
+++ app/src/main/java/com/example/mad_assignment_1/UserCursor.java
@@ -3,6 +3,15 @@ package com.example.mad_assignment_1;
 import android.database.Cursor;
 import android.database.CursorWrapper;
 
+/**
+ * User Cursor Class
+ *
+ * @class           UserCursor
+ * @extends         android.database.CursorWrapper
+ * @author          Tristan S. Tutungis
+ * @date_created    22/09/2022
+ * @last_modified   24/09/2022 21:41
+ */
 public class UserCursor extends CursorWrapper {
     public UserCursor(Cursor cursor) {
         super(cursor);
