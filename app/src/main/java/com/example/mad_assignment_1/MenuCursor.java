5670b1550b1eb768f3db89d0d3237d77b85acec1
Tristan S. Tutungis <19836511@student.curtin.edu.au>
2022-09-24 22:43:13 +0800

    Wrote Menu Cursor class

--- app/src/main/java/com/example/mad_assignment_1/MenuCursor.java
+++ app/src/main/java/com/example/mad_assignment_1/MenuCursor.java
@@ -2,9 +2,30 @@ package com.example.mad_assignment_1;
 
 import android.database.Cursor;
 import android.database.CursorWrapper;
-//TODO: Write this cursor
+
+import com.example.mad_assignment_1.DBSchema.MenuTable;
+
+/**
+ * Menu Cursor Class
+ *
+ * @class           MenuCursor
+ * @extends         android.database.CursorWrapper
+ * @author          Tristan S. Tutungis
+ * @date_created    22/09/2022
+ * @last_modified   24/09/2022 22:40
+ */
 public class MenuCursor extends CursorWrapper {
     public MenuCursor(Cursor cursor) {
         super(cursor);
+    }
+    
+    public Dish getDish()
+    {
+        return new Dish(
+                getInt(getColumnIndex(MenuTable.Columns.IMG)),
+                getString(getColumnIndex(MenuTable.Columns.ITEM)),
+                getString(getColumnIndex(MenuTable.Columns.DESC)),
+                getDouble(getColumnIndex(MenuTable.Columns.PRICE))
+        );
     }
 }
