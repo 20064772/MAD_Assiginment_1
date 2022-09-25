eece9d1f8e62b98ee2c0fe8153b49043e1e41caa
Tristan S. Tutungis <19836511@student.curtin.edu.au>
2022-09-25 10:57:32 +0800

    Wrote Restaurant Cursor

--- app/src/main/java/com/example/mad_assignment_1/RestaurantCursor.java
+++ app/src/main/java/com/example/mad_assignment_1/RestaurantCursor.java
@@ -2,9 +2,26 @@ package com.example.mad_assignment_1;
 
 import android.database.Cursor;
 import android.database.CursorWrapper;
-//TODO: Write this cursor
-public class RestaurantCursor extends CursorWrapper {
+
+import com.example.mad_assignment_1.DBSchema.RestaurantTable;
+/**
+ * Menu Cursor Class
+ *
+ * @class           RestaurantCursor
+ * @extends         android.database.CursorWrapper
+ * @author          Tristan S. Tutungis
+ * @date_created    22/09/2022
+ * @last_modified   25/09/2022 10:57
+ */public class RestaurantCursor extends CursorWrapper {
     public RestaurantCursor(Cursor cursor) {
         super(cursor);
+    }
+    
+    public Restaurant getRestaurant(Menu menu)
+    {
+        return new Restaurant(
+                getString(getColumnIndex(RestaurantTable.Columns.REST)),
+                getInt(getColumnIndex(RestaurantTable.Columns.LOGO)),
+                menu);
     }
 }
