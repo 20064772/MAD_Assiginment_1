92bd4bed985d739a89a0623fed2437dd3889fb90
Tristan S. Tutungis <19836511@student.curtin.edu.au>
2022-09-25 15:17:32 +0800

    Wrote this cursor. Need to finish it once I figure out what to do.

--- app/src/main/java/com/example/mad_assignment_1/OrderHistoryCursor.java
+++ app/src/main/java/com/example/mad_assignment_1/OrderHistoryCursor.java
@@ -2,9 +2,25 @@ package com.example.mad_assignment_1;
 
 import android.database.Cursor;
 import android.database.CursorWrapper;
-//TODO: Write this cursor
+
+/**
+ * Menu Cursor Class
+ *
+ * @class           MenuCursor
+ * @extends         android.database.CursorWrapper
+ * @author          Tristan S. Tutungis
+ * @date_created    22/09/2022
+ * @last_modified   25/09/2022 11:08
+ */
+//TODO: Figure this cursor out
 public class OrderHistoryCursor extends CursorWrapper {
     public OrderHistoryCursor(Cursor cursor) {
         super(cursor);
     }
+    /*
+    public Dish getOrderedDish()
+    {
+    
+    }
+     */
 }
