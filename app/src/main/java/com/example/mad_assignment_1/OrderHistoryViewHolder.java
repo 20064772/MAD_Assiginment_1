de48e2d14ea8923cc52761a238a65e380fcfac2a
Tristan <19836511@student.curtin.edu.au>
2022-09-26 20:16:23 +0800

    Edited order history

--- app/src/main/java/com/example/mad_assignment_1/OrderHistoryViewHolder.java
+++ app/src/main/java/com/example/mad_assignment_1/OrderHistoryViewHolder.java
@@ -37,5 +37,12 @@ public class OrderHistoryViewHolder extends RecyclerView.ViewHolder {
         resName.setText(order.getRestaurant());
         dateTime.setText(order.getDateTime());
         cost.setText(String.valueOf(order.getTotal()));
+        for(Object obj : order.getDishes().getMapBasket().values())
+        {
+            if(obj instanceof BasketDish) {
+                BasketDish bd = (BasketDish) obj;
+                orderHistory.append("Dish: " + bd.getDish().getName() + " Quant: " + bd.getCount() + "\n");
+            }
+        }
     }
 }
